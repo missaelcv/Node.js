56e42f06fbf422544810becd273ae1dc7a211bd4
missaelcv <missaelcv0220@gmail.com>
2021-09-26 21:12:31 -0400

    crear var cowsay

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const {frutas, dinero} = require('./frutas')
+const  cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "Hola Missael",
+    e : "oO",
+    T : "U "
+}));
 
 
 frutas.forEach(item => {
