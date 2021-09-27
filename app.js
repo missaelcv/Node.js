bbe5362bd25c70b0bbd8581d8788c65cfc70e86e
missaelcv <missaelcv0220@gmail.com>
2021-09-26 23:12:19 -0400

    res. 404.html

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.static(__dirname + "/public"));
+
+
+
 app.get('/',(req, res) => {
     res.send('Mi respuesta desde express')
 })
@@ -10,6 +14,12 @@ app.get('/',(req, res) => {
 app.get('/servicios',(req, res) => {
     res.send('Estan en la pagina del servidor')
 })
+
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + "/publc/404.html")
+})
+
 
 app.listen(port, () => {
     console.log('Servidor a su servicio en puerto',port)
