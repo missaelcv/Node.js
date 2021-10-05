9db6cbceec3e77b9cd6905b2bc31ed67a64b056f
missaelcv <missaelcv0220@gmail.com>
2021-10-04 22:23:23 -0400

    nuevo cambios de vist

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
+//Motor de plantilla
+app.set('view engine', 'ejs');
+app.set('views', __dirname + './views');
 
-// Motor de plantilla
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/',(req, res) => {
-    res.render("index", { titulo: "mi titulo dinamico" })
-})
+    res.render("index", {titulo : "Mi Titulo dinamico"});
+});
 
 app.get('/servicios',(req, res) => {
-    res.send('Estan en la pagina del servicios')
-})
+    res.send('Estan en la pagina del servicios');
+});
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + "/publc/404.html")
-})
+    res.status(404).sendFile(__dirname + "/publc/404.html");
+});
 
 app.listen(port, () => {
-    console.log('Servidor a su servicio en puerto',port)
-})
+    console.log('Servidor a su servicio en puerto',port);
+});
