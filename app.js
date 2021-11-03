a09c4462b13db03f929e43700a9ed41508f538e7
missaelcv <missaelcv0220@gmail.com>
2021-11-02 22:05:59 -0400

    Nuevo servidor

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 const express = require('express');
+
 const app = express();
 
 const port = 3000;
-//Motor de plantilla
-app.set('view engine', 'ejs');
-app.set('views', __dirname + './views');
 
+app.set('view engine', 'pug');
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "public"));
 
-app.get('/',(req, res) => {
-    res.render("index", {titulo : "Mi Titulo dinamico"});
+app.get('/', (req, res) => {
+    res.send('Mi Respuesta desde express v.2')
 })
 
-app.get('/servicios',(req, res) => {
-    res.render("servicios", {tituloServicio: "Este es un menesja dinamico de servicios"})
+app.get('/servicios',(req,res) => {
+    res.send('Estas en la pagina de Servicios')
 })
 
-app.use((req, res, next) => {
-    res.status(404).render("404",{
-        titulo: 404,
-        descripcion: "Titulo del sitio web"
-    })
+app.use((req,res,next) => {
+    res.status(404).sendFile(__dirname + "/public/404.html")
 })
 
 app.listen(port, () => {
-    console.log('Servidor a su servicio en puerto',port)
+    console.log('servidor a su servicio en el pierto', port)
 })
